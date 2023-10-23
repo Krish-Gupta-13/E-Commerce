949e6fcfa665fae8a2d68fd3f9198ea182251bf1
Krish Gupta <krishguptaa13@gmail.com>
2023-10-23 21:23:58 +0530

    intial commit

---
+++ src/pages/ProductDetailPage.js
@@ -0,0 +1,13 @@
+import NavBar from "../features/navbar/Navbar";
+import ProductDetails from "../features/product/components/ProductDetails";
+
+function ProductDetailsPage (){
+    return (
+        <div>
+            <NavBar>
+                <ProductDetails></ProductDetails>
+            </NavBar>
+        </div>
+    );
+}
+export default ProductDetailsPage;
